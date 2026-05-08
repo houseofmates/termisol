e857f3055514dc3414ac7b631491ab02653600ea
pkm-sync <sync@houseofmates.space>
2026-05-08 02:55:18 -0700

    Auto-commit: Update check_api.dart

--- check_api.dart
+++ check_api.dart
@@ -1,9 +1,6 @@
 import 'dart:ui' as ui;
-import 'dart:typed_data';
 
 void main() async {
   final program = await ui.FragmentProgram.fromAsset('test');
-  final shader = program.fragmentShader(
-    floatUniforms: Float32List(0),
-  );
+  final shader = ui.FragmentShader.fromFloatUniforms(program, []);
 }

6c5ce4a43683b5eaaa286cc35704cb0ce2bffe6f
pkm-sync <sync@houseofmates.space>
2026-05-08 14:22:20 -0700

    Auto-commit: Update MainActivity.kt

--- android/app/src/main/kotlin/com/termisol/MainActivity.kt
+++ android/app/src/main/kotlin/com/termisol/MainActivity.kt
@@ -6,24 +6,15 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
-import kotlinx.coroutines.*
 
 class MainActivity : FlutterActivity() {
-    private val TAG = "Termisol"
-    private val CHANNEL = "com.termisol/vr"
-    private val HAND_TRACKING_CHANNEL = "com.termisol/vr/hand_tracking"
-    private val EYE_TRACKING_CHANNEL = "com.termisol/vr/eye_tracking"
-    private val DEVICE_DETECTION_CHANNEL = "com.termisol/vr/device_detection"
-
-    private var handTrackingSink: EventChannel.EventSink? = null
-    private var eyeTrackingSink: EventChannel.EventSink? = null
-    private var deviceDetectionSink: EventChannel.EventSink? = null
-
-    private val scope = CoroutineScope(Dispatchers.Main + SupervisorJob())
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    companion object {
+        private const val TAG = "Termisol"
+        private const val CHANNEL = "com.termisol/vr"
+        private const val DEVICE_DETECTION_CHANNEL = "com.termisol/vr/device_detection"
     }
+
+    private var deviceDetectionSink: EventChannel.EventSink? = null
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -39,28 +30,6 @@ class MainActivity : FlutterActivity() {
             }
         }
 
-        EventChannel(flutterEngine.dartExecutor.binaryMessenger, HAND_TRACKING_CHANNEL).setStreamHandler(
-            object : EventChannel.StreamHandler {
-                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                    handTrackingSink = events
-                }
-                override fun onCancel(arguments: Any?) {
-                    handTrackingSink = null
-                }
-            }
-        )
-
-        EventChannel(flutterEngine.dartExecutor.binaryMessenger, EYE_TRACKING_CHANNEL).setStreamHandler(
-            object : EventChannel.StreamHandler {
-                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                    eyeTrackingSink = events
-                }
-                override fun onCancel(arguments: Any?) {
-                    eyeTrackingSink = null
-                }
-            }
-        )
-
         EventChannel(flutterEngine.dartExecutor.binaryMessenger, DEVICE_DETECTION_CHANNEL).setStreamHandler(
             object : EventChannel.StreamHandler {
                 override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
@@ -75,27 +44,22 @@ class MainActivity : FlutterActivity() {
     }
 
     private fun getDeviceInfo(): Map<String, Any> {
+        val display = windowManager.defaultDisplay
+        val refreshRate = display.refreshRate
         return mapOf(
             "deviceType" to "android",
             "supportsHandTracking" to false,
             "supportsEyeTracking" to false,
             "supportsSpatialAudio" to false,
-            "displayRefreshRate" to 60.0
+            "displayRefreshRate" to refreshRate
         )
     }
 
     private fun sendDeviceInfo() {
-        scope.launch {
-            try {
-                deviceDetectionSink?.success(getDeviceInfo())
-            } catch (e: Exception) {
-                Log.e(TAG, "Failed to send device info", e)
-            }
+        try {
+            deviceDetectionSink?.success(getDeviceInfo())
+        } catch (e: Exception) {
+            Log.e(TAG, "Failed to send device info", e)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        scope.cancel()
     }
 }

f919c1feab22a979525173c4948be60c00d1253a
pkm-sync <sync@houseofmates.space>
2026-05-07 17:04:09 -0700

    Auto-commit: Update 2 files

--- lib/core/termisol_plugin_system.dart
+++ lib/core/termisol_plugin_system.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:collection';
+import 'dart:convert';
+import 'dart:io';
 import 'dart:isolate';
 import 'dart:typed_data';
 import 'package:flutter/foundation.dart';

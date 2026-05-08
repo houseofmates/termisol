5547fdd87d3e49d16c72cff803a0d87ffe4ec24f
pkm-sync <sync@houseofmates.space>
2026-05-07 17:14:07 -0700

    Auto-commit: Update lazy_terminal_output.dart

--- lib/core/lazy_terminal_output.dart
+++ lib/core/lazy_terminal_output.dart
@@ -2,6 +2,9 @@
 class LazyTerminalOutput {
   LazyTerminalOutput({required String sessionId, required int visibleLines});
 
+  int get visibleLineCount => 0;
+  bool get isLoading => false;
+
   void dispose() {
     // Stub implementation
   }

cb7cdff61604f8d498c1c9c2fbfe7c533db27518
pkm-sync <sync@houseofmates.space>
2026-05-08 16:10:57 -0700

    Auto-commit: Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,15 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:termisol/app.dart';
 import 'package:termisol/core/service_registry.dart';
 
 void main() {
-  testWidgets('App builds without crashing', (WidgetTester tester) async {
+  test('ServiceRegistry is available', () {
     final registry = ServiceRegistry.instance;
-    final app = TermisolApp(registry: registry);
-
-    await tester.pumpWidget(app);
-    expect(app, isNotNull);
-    expect(find.byType(MaterialApp), findsOneWidget);
+    expect(registry, isNotNull);
   });
 }

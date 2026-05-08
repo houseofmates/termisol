11267236ea550faf772da0836fecb2afb9b0dbee
pkm-sync <sync@houseofmates.space>
2026-05-08 00:29:10 -0700

    Auto-commit: Update 5 files

---
+++ lib/core/neural_processing_system.dart
@@ -0,0 +1,4 @@
+// Stub implementation for NeuralProcessingSystem
+class NeuralProcessingSystem {
+  // Stub implementation
+}
---
+++ lib/ui/terminal_pane_manager.dart
@@ -0,0 +1,4 @@
+// Stub implementation for TerminalPaneManager
+class TerminalPaneManager {
+  // Stub implementation
+}

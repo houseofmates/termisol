99aa16580ad91b5833f78084f9b0ef01680c2180
pkm-sync <sync@houseofmates.space>
2026-05-07 17:14:33 -0700

    Auto-commit: Update 2 files

--- lib/core/optimized_text_buffer.dart
+++ lib/core/optimized_text_buffer.dart
@@ -2,6 +2,9 @@
 class OptimizedTextBuffer {
   OptimizedTextBuffer({required int maxLines});
 
+  int get _cursorPosition => 0;
+  Map<String, dynamic> get stats => {};
+
   void clear() {
     // Stub implementation
   }

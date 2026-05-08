2f07d4e452e0865d3ccec2cdd787eb261640c36c
pkm-sync <sync@houseofmates.space>
2026-05-08 00:41:58 -0700

    Auto-commit: Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:async';
 import 'dart:convert';
 import 'dart:ui';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:window_manager/window_manager.dart';

f902f9dff50517389063de18f407ce007a1d2d0f
pkm-sync <sync@houseofmates.space>
2026-05-08 16:10:34 -0700

    Auto-commit: Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,15 +4,12 @@ import 'package:termisol/app.dart';
 import 'package:termisol/core/service_registry.dart';
 
 void main() {
-  testWidgets('App builds and renders', (WidgetTester tester) async {
+  testWidgets('App builds without crashing', (WidgetTester tester) async {
     final registry = ServiceRegistry.instance;
     final app = TermisolApp(registry: registry);
 
     await tester.pumpWidget(app);
-    await tester.pumpAndSettle();
-
     expect(app, isNotNull);
-    // HomeScreen renders a scaffold with terminal tabs
     expect(find.byType(MaterialApp), findsOneWidget);
   });
 }

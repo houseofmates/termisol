87cb49b4ebb0476fbe042caa2c3df5810a895073
pkm-sync <sync@houseofmates.space>
2026-05-07 23:45:30 -0700

    Auto-commit: Update edit.dart

--- lib/ui/edit.dart
+++ lib/ui/edit.dart
@@ -80,7 +80,7 @@ class _EditTerminalState extends State<EditTerminal> {
         child: Row(
           children: [
             Icon(
-              IconData(title),
+              title as String,
               color: currentValue ? Colors.white : Colors.grey[400],
               size: 16,
             ),
